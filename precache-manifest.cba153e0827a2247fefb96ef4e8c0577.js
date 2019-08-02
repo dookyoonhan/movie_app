2b5686df84c7a44d51035d2fc6334aeff7a27daa
dk <dkhan@mavlux.com>
2019-08-02 16:05:08 +0900

    Updates

---
+++ precache-manifest.cba153e0827a2247fefb96ef4e8c0577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb561f5218a71a9b8e845dcade2aa5b3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5750137e20e56245fd31",
+    "url": "/movie_app/static/css/main.eeb6c324.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/movie_app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "5750137e20e56245fd31",
+    "url": "/movie_app/static/js/main.fca3d94a.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
